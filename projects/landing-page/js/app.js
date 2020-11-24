1e9e2da3118ad8604059fddfd801f0400c0f42bd
Cheolho Park <daeji77@gmail.com>
2020-11-23 21:54:18 -0800

    Navigate to section on click

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -34,23 +34,41 @@
 */
 
 // build the nav
-const fragement = new DocumentFragment();
-const sections = document.querySelectorAll('section');
-for (section of sections) {
-    const li = document.createElement('li');
-    li.classList.add('menu__link');
-    li.textContent = section.getAttribute('data-nav')
-    // console.log(section.getAttribute('data-nav'));
-    fragement.appendChild(li);
+function buildNav() {
+    const fragement = new DocumentFragment();
+    const sections = document.querySelectorAll('section');
+    for (section of sections) {
+        const li = document.createElement('li');
+        li.classList.add('menu__link');
+        li.textContent = section.getAttribute('data-nav');
+        li.setAttribute('data-nav-id', section.id);
+
+        // const a = document.createElement('A');
+        // a.textContent = section.getAttribute('data-nav');
+        // console.log(`#${section.id}`);
+        // a.href = `#${section.id}`;
+        // li.appendChild(a);
+
+        fragement.appendChild(li);
+    }
+    const navbar = document.querySelector('#navbar__list');
+    navbar.appendChild(fragement);
 }
-const navbar = document.querySelector('#navbar__list');
-navbar.appendChild(fragement);
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
+function scrollToElem(elem) { 
+    const pageHeaderBottom = document.querySelector('.page__header');
+    const topPos = elem.getBoundingClientRect().top + window.pageYOffset -
+        pageHeaderBottom.getBoundingClientRect().bottom
 
+    window.scrollTo({
+      top: topPos,
+      behavior: 'smooth'
+    })
+}
 
 /**
  * End Main Functions
@@ -59,9 +77,14 @@ navbar.appendChild(fragement);
 */
 
 // Build menu 
+buildNav();
 
 // Scroll to section on link click
+const navbar = document.querySelector('#navbar__list');
+navbar.addEventListener('click', function(event) {
+   const id = event.target.getAttribute('data-nav-id');
+   const section = document.querySelector(`#${id}`);
+   scrollToElem(section);
+})
 
 // Set sections as active
-
-
